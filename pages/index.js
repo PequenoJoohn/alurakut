d7ec5490a786cffa58b9cc9cb714b5147af4a563
jonathanlm <jonathan.matos@frncubo.com>
2021-07-12 21:32:14 -0300

    feat: add image

--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,53 @@ const Box = styled.div`
   border-radius: 8px;
 `;
 
+const MainGrid = styled.main`
+  width: 100%;
+  grid-gap: 10px;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 500px;
+  padding: 16px;
+  display: grid;
+  .profileArea {
+    display: none;
+    @media screen and (min-width: 860px) {
+      display: block;
+    }
+  }
+  @media screen and (min-width: 860px) {
+    max-width: 1110px;
+    display: grid;
+    grid-template-areas: 
+      "profileArea welcomeArea profileRelationsArea";
+    grid-template-columns: 160px 1fr 312px;
+  }
+`;
+
 export default function Home() {
   return (
-    <main>
+    <MainGrid>
+      <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+        <Box>
+          <img src="https://github.com/pequenojoohn.png" alt="" />
+        </Box>
+      </div>
+      <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+        <Box>
+          Bem vindo
+        </Box>
+      </div>
+      <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+        <Box>
+          Pessoas da comunidade
+        </Box>
+        <Box>
+          Meus Amigos
+        </Box>
+      </div>
 
 
-    </main>
+
+    </MainGrid>
   )
 }
